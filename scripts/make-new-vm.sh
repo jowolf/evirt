a875bb227ead1009087a56e7afccfe187ed3f7f9
Joseph Wolff <joe@osdf.com>
2016-06-19 16:45:16 -0700

    Update make_new_vm.sh with changes through Jan 2016 - JJW

--- scripts/make-new-vm.sh
+++ scripts/make-new-vm.sh
@@ -1,3 +1,7 @@
+# JJW Dec 2015 - copy image fm salt cache
+#
+# JJW Jan 5 2016 - quote pw literal, has $ in it (!)
+#
 # JJW Nov 2015 - create new VM from:
 # - IP addr last tumbler (defaults to current dir basename)
 # - hostname
@@ -5,9 +9,15 @@
 #
 # using templates from metadata.yaml, useredata.yaml, and make-cloudinit.sh
 
+image=/var/cache/salt/minion/extrn_files/base/cloud-images.ubuntu.com/releases/14.04/14.04.3/ubuntu-14.04-server-cloudimg-amd64-disk1.img
 iplast=$(basename `pwd`)
-host=seafile
+host=tlg
 size=50G
+
+if ( ls -l $host.img ); then
+  echo IMAGE PRESENT - WOULD OVERWRITE
+  exit;
+fi
 
 
 cat >metadata.yaml <<EOF
@@ -37,7 +47,7 @@ users:
     primary-group: joe
     groups: [users, root, kvm, sudo, adm, audio, cdrom, dialout, floppy, video, plugdev, dip, netdev]
     lock-passwd: false
-    passwd: $6$rounds=4096$9HeOrRyXEKcTYA5h$mjWahfdV6X.lIiyDwRc/6hXv.7UJCH55XZbHgOOTG3yRh/3vOg5iEukKwYV8kURYtE.GuE/SGI0sUCSAqAgkS.
+    passwd: '$6$rounds=4096$9HeOrRyXEKcTYA5h$mjWahfdV6X.lIiyDwRc/6hXv.7UJCH55XZbHgOOTG3yRh/3vOg5iEukKwYV8kURYtE.GuE/SGI0sUCSAqAgkS.'
     ssh-authorized-keys:
       - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+xUBBvQ7+5WiKLSsBUrIbqyecadH+FFSJzrvA43hxM+z1LP5G0CnMHFVIYF68b58rzPvWurYshCRwOf6Z0ZOc0IAqyOxRQeIG6hphT5TfL+gB+h/BJ+YaWxNR0s7EJYr/2hWUP0j1xJ6EFt1EUH9p5vi4tRo1NcX7syxQUmedtkLrOgM7p5wAbcNGkjog8SmMyHyMZD5yQPt7kbkz2qUmZzf2CNR4aUZEoJnvyLoXDrz1OxZPklcjeXVUH7w91WKPIrTm+lt4xOn0XuCqmHzIlNixyHTBOrdoDuRbejhn/UOFswb3YFRMLczK6f3N+UqqpV9PAErK9QGOAyEjLRHZ root@mintstudio-maya
     sudo: ALL=(ALL) NOPASSWD:ALL
@@ -62,8 +72,8 @@ permanent=1
 parms="-spice port=$((33000+$iplast)),password=meatballs"
 EOF
 
-# could also copy from image locate here:
-mv ubuntu-14.04-server-cloudimg-amd64-disk1.img $host.img
+# copy from image location:
+cp $image $host.img
 
 qemu-img resize $host.img $size
 
