df0bb636b68d3fb677196fd53eda41c9120bd9a5
Joseph Wolff <joe@osdf.com>
2018-06-22 14:52:47 -0700

    Interim WIP - setup command, work in an actual VM dir - JJW

---
+++ new/setup.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+sudo apt install 	\
+    virtualenv 		\
+    qemu-kvm 		\
+    qemu-utils 		\
+    spice-client-gtk 	\
+    spice-html5 	\
+    remmina-plugin-spice \
+    rdesktop		\
+    vinagre		\
+    libguestfs-tools	\
+    cloud-image-utils	\
+
+
+# Also other rdp clients
+# Also other vnc clients - xtightvncviewer, xvnc4viewer
+# Also other remmina plugins
+# Also sudo apt install qemuctl # for qt4 GUI
+
+sudo adduser joe kvm
+
+if ! test -d env; then {
+  virtualenv --python=python3 env/
+  env/bin/pip install -r requirements.txt
+} fi
+
+
+#sudo ln -sf $(pwd)/ev.py /usr/local/bin/evm
+
+echo "#! $(pwd)/env/bin/python $(pwd)/ev.py" >evm
+chmod +x evm
+sudo ln -sf $(pwd)/ev /usr/local/bin/evm
+
+echo DONE
+
+sudo ./stuffit.sh . env/bin/activate
